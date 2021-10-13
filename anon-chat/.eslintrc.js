d9ddd3a822139d6ae65156557fce7f0300d9301b
dgop92 <inevaup@gmail.com>
2021-10-12 22:17:44 -0500

    refactor: add prettier rule to eslint config

--- anon-chat/.eslintrc.js
+++ anon-chat/.eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:jsx-a11y/recommended",
         "plugin:react-hooks/recommended",
-        "airbnb"
+        "airbnb",
+        "prettier",
     ],
     "parserOptions": {
         "ecmaFeatures": {
--- anon-chat/src/components/Lobby.jsx
+++ anon-chat/src/components/Lobby.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import { TextField, Typography, Paper } from "@mui/material";
 import PrimaryButton from "./base/PrimaryButton";
 
-const BACKGROUND_IMAGE_URL = "https://source.unsplash.com/CdBPMI26KDo/1920x1280";
+const BACKGROUND_IMAGE_URL =
+  "https://source.unsplash.com/CdBPMI26KDo/1920x1280";
 
 export default function Lobby() {
   return (
@@ -16,26 +17,51 @@ export default function Lobby() {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <Box elevation={1} m={2} p={3} sx={{ maxWidth: 800 }} component={Paper}>
-        <Typography sx={{ fontWeight: 700 }} mb={2} variant="h4" component="h4">
-          Anon Chat
-        </Typography>
-        <Typography mb={2} variant="body1" component="p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-          ratione sapiente qui, voluptas totam a molestias est. Quisquam, aut.
-          Cupiditate eaque atque culpa ex maiores?
-        </Typography>
+      <LobbyCard />
+    </Box>
+  );
+}
+
+function LobbyCard() {
+  const [name, setName] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const onSubmit = (event) => {
+    console.log(name);
+    event.preventDefault();
+  };
+
+  return (
+    <Box elevation={1} m={2} p={3} sx={{ maxWidth: 800 }} component={Paper}>
+      <Typography sx={{ fontWeight: 700 }} mb={2} variant="h4" component="h4">
+        Anon Chat
+      </Typography>
+      <Typography mb={2} variant="body1" component="p">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ratione
+        sapiente qui, voluptas totam a molestias est. Quisquam, aut. Cupiditate
+        eaque atque culpa ex maiores?
+      </Typography>
+      <form onSubmit={onSubmit}>
         <TextField
           fullWidth
-          id="outlined-basic"
+          id="chatName"
           label="Nombre"
           variant="outlined"
           size="small"
+          value={name}
+          onChange={handleNameChange}
+          inputProps={{
+            required: true,
+          }}
         />
         <Box mt={2} sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <PrimaryButton variant="contained">Join</PrimaryButton>
+          <PrimaryButton type="submit" variant="contained">
+            Join
+          </PrimaryButton>
         </Box>
-      </Box>
+      </form>
     </Box>
   );
 }
